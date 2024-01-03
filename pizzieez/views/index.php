0bfcde650377749e5ee028aaaacd6c5186da34f8
Aghnia Nurhidayah <aghnianurhidayah@gmail.com>
2024-01-03 12:46:30 +0800

    upload fixed posttest project

---
+++ pizzieez/views/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PIZZIEEZ</title>
+
+    <link rel="stylesheet" href="../styles/style.css">
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+
+    <div class="hero">
+        <div class="hero-img">
+            <div class="img-overlay"></div>
+        </div>
+        <div class="hero-content">
+            <p>LET'S GET A SLICE OF PIZZIEEZ!</p>
+            <div class="tagline">
+                <ul>
+                    <li>TASTY</li>
+                    <li>MEATY</li>
+                    <li>FRESH</li>
+                </ul>
+            </div>
+            <div class="order-btn">
+                <a class="button" href="">Order Now</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="fav-menu">
+            <h1>OUR FAVORITES MENU</h1>
+            <div class="boxes">
+                <div class="box">
+                    <img src="../assets/img/pizza-1.png" alt="" />
+                    <div class="menu">
+                        <p>PIZZA 1</p>
+                        <p>$20</p>
+                    </div>
+                    <p>Paperoni, Mozarella Cheese, Tomato Sauce</p>
+                    <!-- <a href="">Order</a> -->
+                </div>
+                <div class="box">
+                    <img src="../assets/img/pizza-2.png" alt="" />
+                    <div class="menu">
+                        <p>PIZZA 2</p>
+                        <p>$20</p>
+                    </div>
+                    <p>Cheedar Cheese, Mozarella Cheese, Tomato Sauce</p>
+                </div>
+                <div class="box">
+                    <img src="../assets/img/pizza-3.png" alt="" />
+                    <div class="menu">
+                        <p>PIZZA 3</p>
+                        <p>$20</p>
+                    </div>
+                    <p>Chicken Meat, Mushroom, Tomato, BBQ Sauce</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
---
+++ pizzieez/views/navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../assets/fontawesome-free-6.5.1-web/css/all.min.css">
+</head>
+<body>
+    <nav>
+        <div class="nav-logo">
+            <p>PIZZIEEZ</p>
+        </div>
+        <div class="menu-icon" id="menu-icon">
+            <i class="fa-solid fa-bars"></i>
+        </div>   
+        <ul class="d-none" id="menu-list">
+            <li><a href="">Home</a></li>
+            <li><a href="">Menu</a></li>
+            <li><a href="">My Order</a></li>
+            <li><a href="">About</a></li>
+        </ul>
+    </nav>
+
+    <script src="../scripts/script.js"></script>
+</body>
+</html>
